f7ceebbdd3e2048444ddd62e0ab406eb79f02e4b
villa <matias.villalemes@metropolia.fi>
2024-11-01 13:24:24 +0200

    02 valmis

--- 02_consoleapi.js
+++ 02_consoleapi.js
@@ -1,15 +1,23 @@
 'use strict';
-const airportForm = document.getElementById('query');
-airportForm.addEventListener('submit', async function(evt) {
 
+const airportForm = document.querySelector('form');
+airportForm.addEventListener('submit', async function(evt) {
     evt.preventDefault();
 
     const code = document.getElementById('query').value;
     try {
+
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
         const jsonData = await response.json();
+
         console.log(jsonData);
     } catch (error) {
-        console.log(error.message);
+
+        console.log(`Error: ${error.message}`);
     }
 });
